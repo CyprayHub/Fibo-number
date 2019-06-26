653a7eec516bfe24575a9ce9c482d439e8d08406
CyprayHub <51928069+CyprayHub@users.noreply.github.com>
2019-06-26 00:27:26 -0700

    Update FiboController.java

--- VS_FIBO_NUMBER/fibo-api/src/main/java/vs/springbootproject/api/controller/FiboController.java
+++ VS_FIBO_NUMBER/fibo-api/src/main/java/vs/springbootproject/api/controller/FiboController.java
@@ -15,30 +15,22 @@ import vs.springbootproject.api.model.Fibonumbers;
 import vs.springbootproject.api.service.FiboService;
 
 @RestController
-
-public class FiboController{
+public class FiboController {
 	Fibonumbers fibonumbers;
 	@Autowired
 	private FiboService fiboService;
 	public static final Logger logger = Logger.getLogger(Class.class.getName());
-	
-	
+
 	@RequestMapping("/fibnumber/{number}")
 	public ArrayList<Integer> findNumber(@PathVariable int number, HttpServletResponse response) throws IOException {
-		
-		
-		if(number < 0) {
-			
+
+		if (number < 0) {
+
 			response.sendError(400, "Please enter a number that is greater than zero");
-			
-					
+
 		}
-          
-       
-       return fiboService.findNumber(number);
+
+		return fiboService.findNumber(number);
 	}
-	
-	
-   
 
 }
